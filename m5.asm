bae990e37302ce7ce2948f8b76215d000f7fd20b
Charlie Robson <charlie.robson@b2m-solutions.com>
2019-11-07 14:11:39 +0000

    initial commit

---
+++ m5.asm
@@ -0,0 +1,39 @@
+; WAS:
+;
+; SEGA Master System Music Interface - Z80 code
+; By Sebastian Tomczak
+
+; NOW:
+
+; Sord M5 Music Interface 
+; By Charlie 'SirMorris' Robson
+
+	.org	$2000
+
+	di						; no interrupts please, we're british
+
+	ld		sp,$7fff
+	ld		hl,$8000
+
+Loop:
+	ld		a,(hl)
+	bit		7,a				; bit 7 signals a write
+	jr		nz,Loop
+
+	bit		6,a				; nibble is a high nibble if bit 7 set
+	jr		z,LowNibble
+
+	sla		a				; shift the nibble into high bits
+	sla		a
+	sla		a
+	sla		a
+	or		b
+	out		($20),a			; send the data to the PSG 
+	jr		Loop
+
+LowNibble:
+	and		$0f
+	ld		b,a				; stash
+	jr		Loop
+
+	.ds		$4000-$
